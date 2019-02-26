fa5055e37e93eaad0d92f0a0d1e105322ba906c0
Roben Kleene <contact@robenkleene.com>
2019-02-26 12:16:50 -0500

    Run autocorrect

--- SevenHup/SUPProcessMonitor.m
+++ SevenHup/SUPProcessMonitor.m
@@ -18,18 +18,16 @@
 
 @implementation SUPProcessMonitor
 
-- (instancetype)initWithIdentifier:(int)identifier
-{
+- (instancetype)initWithIdentifier:(int)identifier {
     self = [super init];
     if (self != nil) {
         _identifier = identifier;
     }
-    
+
     return self;
 }
 
-- (void)watchWithCompletionHandler:(void (^)(BOOL success))completionHandler
-{
+- (void)watchWithCompletionHandler:(void (^)(BOOL success))completionHandler {
     self.completionHandler = completionHandler;
 
     int queue = kqueue();
@@ -37,66 +35,63 @@
         [self fireCompletionHandlerWithResult:NO];
         return;
     }
-    
+
     pid_t identifier = self.identifier;
     struct kevent changes;
     EV_SET(&changes, identifier, EVFILT_PROC, EV_ADD | EV_RECEIPT, NOTE_EXIT, 0, NULL);
-    
+
     if (kevent(queue, &changes, 1, &changes, 1, NULL) == -1) {
         [self fireCompletionHandlerWithResult:NO];
         return;
     }
-    
-    CFFileDescriptorContext context = { 0, (void *)CFBridgingRetain(self), NULL, NULL, NULL };
+
+    CFFileDescriptorContext context = {0, (void *)CFBridgingRetain(self), NULL, NULL, NULL};
     CFFileDescriptorRef noteExitKQueueRef = CFFileDescriptorCreate(NULL, queue, true, noteExitKQueueCallback, &context);
     if (noteExitKQueueRef == NULL) {
         CFRelease((__bridge CFTypeRef)(self));
         [self fireCompletionHandlerWithResult:NO];
         return;
     }
-    
+
     CFRunLoopSourceRef runLoopSource = CFFileDescriptorCreateRunLoopSource(NULL, noteExitKQueueRef, 0);
     if (runLoopSource == NULL) {
         CFRelease((__bridge CFTypeRef)(self));
         [self fireCompletionHandlerWithResult:NO];
         return;
     }
-    
+
     CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, kCFRunLoopDefaultMode);
     CFRelease(runLoopSource);
-    
+
     CFFileDescriptorEnableCallBacks(noteExitKQueueRef, kCFFileDescriptorReadCallBack);
-    
+
     if (self.isTerminated) {
         [self fireCompletionHandlerWithResult:YES];
     }
 }
 
-static void noteExitKQueueCallback(CFFileDescriptorRef file, CFOptionFlags __unused callBackTypes, void *info)
-{
+static void noteExitKQueueCallback(CFFileDescriptorRef file, CFOptionFlags __unused callBackTypes, void *info) {
     struct kevent event;
     kevent(CFFileDescriptorGetNativeDescriptor(file), NULL, 0, &event, 1, NULL);
-    
+
     SUPProcessMonitor *processMonitor = CFBridgingRelease(info);
     processMonitor.isTerminated = YES;
     [processMonitor fireCompletionHandlerWithResult:YES];
 }
 
-- (BOOL)isRunning
-{
-	if (self.isTerminated) {
-		return YES;
-	}
+- (BOOL)isRunning {
+    if (self.isTerminated) {
+        return YES;
+    }
 
     return kill(self.identifier, 0) == 0;
 }
 
-- (void)fireCompletionHandlerWithResult:(BOOL)success
-{
+- (void)fireCompletionHandlerWithResult:(BOOL)success {
     if (!self.completionHandler) {
         return;
     }
-	
+
     void (^completionHandler)(BOOL) = self.completionHandler;
     self.completionHandler = nil;
     completionHandler(success);
