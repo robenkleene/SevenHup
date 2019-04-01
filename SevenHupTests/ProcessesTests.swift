1faeb91fbca39a0f56653565eacd51688531648c
Roben Kleene <contact@robenkleene.com>
2019-04-01 16:02:53 -0400

    Migrate over similar test

--- SevenHupTests/ProcessesTests.swift
+++ SevenHupTests/ProcessesTests.swift
@@ -8,26 +8,58 @@
 
 import XCTest
 
+@testable import SevenHup
+import SodaStream
+
+
 class ProcessesTests: XCTestCase {
+    func testWithProcesses() {
+        var tasks = [Process]()
+        for _ in 0 ... 2 {
+            let commandPath = path(forResource: testDataShellScriptCatName,
+                                   ofType: testDataShellScriptExtension,
+                                   inDirectory: testDataSubdirectory)!
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+            let runExpectation = expectation(description: "Task ran")
+            let task = SDATaskRunner.runTask(withCommandPath: commandPath,
+                                             withArguments: nil,
+                                             inDirectoryPath: nil,
+                                             delegate: nil) { (success) -> Void in
+                XCTAssertTrue(success)
+                runExpectation.fulfill()
+            }
+            tasks.append(task)
         }
-    }
+        waitForExpectations(timeout: testTimeout, handler: nil)
 
+        let taskIdentifiers = tasks.map { $0.processIdentifier }.sorted { $0 < $1 }
+        let processFilterExpectation = expectation(description: "Filter processes")
+
+        ProcessFilter.runningProcesses(withIdentifiers: taskIdentifiers) { (identifierToProcessData, error) -> Void in
+            guard let identifierToProcessData = identifierToProcessData else {
+                XCTAssertTrue(false)
+                return
+            }
+            XCTAssertNil(error)
+
+            XCTAssertEqual(identifierToProcessData.count, 3)
+
+            let processIdentifiers = identifierToProcessData.values.map({ $0.identifier }).sorted { $0 < $1 }
+            XCTAssertEqual(processIdentifiers, taskIdentifiers)
+            processFilterExpectation.fulfill()
+        }
+        waitForExpectations(timeout: testTimeout, handler: nil)
+
+        // Clean up
+
+        for task in tasks {
+            let interruptExpectation = expectation(description: "Interrupt finished")
+            task.wcl_interrupt { (success) -> Void in
+                XCTAssertTrue(success)
+                interruptExpectation.fulfill()
+            }
+        }
+
+        waitForExpectations(timeout: testTimeout, handler: nil)
+    }
 }
