e19d0b32c9cfa7e64895568f28cff673d40eeaa3
Roben Kleene <contact@robenkleene.com>
2019-04-03 18:07:37 -0400

    Compartmentalize code

--- SevenHup/Processes.swift
+++ SevenHup/Processes.swift
@@ -9,9 +9,41 @@
 import Foundation
 
 class Processes {
-    class func processInfos(for identifiers: [pid_t]) -> [Any] {
+    class func runningProcesses(withIdentifiers identifiers: [Int32],
+                                completionHandler: @escaping ((_ identifierToProcessData: [Int32: ProcessData]?,
+        _ error: NSError?) -> Void)) {
+        if identifiers.isEmpty {
+            completionHandler([Int32: ProcessData](), nil)
+            return
+        }
+        DispatchQueue.global(qos: .background).async {
+            let dictionaries = Processes.processInfos(for: identifiers)
+            let processDatas = makeProcessDatas(dictionaries: dictionaries)
+            completionHandler(processDatas, nil)
+        }
+    }
+
+    // MARK: Private
+
+    private class func processInfos(for identifiers: [pid_t]) -> [Any] {
         let identifiersSet = Set(identifiers)
         let identifierToProcesses = SUPProcesses.identifierToProcesses(forIdentifiers: identifiersSet as Set<NSNumber>)
         return Array(identifierToProcesses.values)
     }
+    
+    private class func makeProcessDatas(dictionaries: [Any]) -> [Int32: ProcessData] {
+        guard let processDictionaries = dictionaries as? [NSDictionary] else {
+            assert(false)
+            return [Int32: ProcessData]()
+        }
+        
+        var identifierToProcessData = [Int32: ProcessData]()
+        for dictionary in processDictionaries {
+            if let processData = ProcessData.makeProcessData(dictionary: dictionary) {
+                identifierToProcessData[processData.identifier] = processData
+            }
+        }
+        return identifierToProcessData
+    }
+
 }
