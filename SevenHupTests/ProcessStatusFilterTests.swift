d4b661614e4a5bf3d02c6bead45dd0a51a07ad48
Roben Kleene <contact@robenkleene.com>
2019-04-04 12:50:28 -0400

    Start fixing tests

--- SevenHupTests/ProcessStatusFilterTests.swift
+++ SevenHupTests/ProcessStatusFilterTests.swift
@@ -6,74 +6,88 @@
 //  Copyright © 2015 Roben Kleene. All rights reserved.
 //
 
-//@testable import SevenHup
-//import SodaStream
-//import XCTest
-//
-//class ProcessStatusFilterNoProcessTests: XCTestCase {
-//    lazy var testProcessData: ProcessData = {
-//        let identifier = Int32(74)
-//        let dateFormatter = DateFormatter()
-//        dateFormatter.dateFormat = "EEE MMM d HH:mm:ss yyyy"
-//        let startTime = dateFormatter.date(from: "Wed Dec 16 02:09:32 2015")!
-//        let commandPath = "/usr/libexec/wdhelper"
-//        return ProcessData(identifier: identifier,
-//                           startTime: startTime,
-//                           commandPath: commandPath)!
-//    }()
-//
-//    func testEmptyIdentifiers() {
-//        let expectation = self.expectation(description: "Process filter finished")
-//        ProcessStatusFilter.runningProcesses(withIdentifiers: [Int32]()) { (identifierToProcessData, error) -> Void in
-//            XCTAssertNotNil(error)
-//            XCTAssertNil(identifierToProcessData)
-//            expectation.fulfill()
-//        }
-//        waitForExpectations(timeout: testTimeout, handler: nil)
-//    }
-//
-//    func testEmptyInput() {
-//        var processDatas = ProcessStatusFilter.makeProcessDatas(output: "")
-//        XCTAssertEqual(processDatas.count, 0)
-//        processDatas = ProcessStatusFilter.makeProcessDatas(output: " ")
-//        XCTAssertEqual(processDatas.count, 0)
-//    }
-//
-//    func testExampleInput() {
-//        let fileURL = url(forResource: testDataTextPSOutputSmall,
-//                          withExtension: testDataTextExtension,
-//                          subdirectory: testDataSubdirectory)!
-//
-//        let output = makeString(contentsOf: fileURL)!
-//
-//        let identifierToProcessData = ProcessStatusFilter.makeProcessDatas(output: output)
-//        XCTAssertEqual(identifierToProcessData.count, 3)
-//        guard let processData = identifierToProcessData[testProcessData.identifier] else {
-//            XCTAssertTrue(false)
-//            return
-//        }
-//
-//        XCTAssertEqual(processData.identifier, testProcessData.identifier)
-//        XCTAssertEqual(processData.startTime, testProcessData.startTime)
-//        XCTAssertEqual(processData.commandPath, testProcessData.commandPath)
-//    }
-//
-//    func testBadExampleInput() {
-//        let fileURL = url(forResource: testDataTextPSOutputBad,
-//                          withExtension: testDataTextExtension,
-//                          subdirectory: testDataSubdirectory)!
-//
-//        let output = makeString(contentsOf: fileURL)!
-//
-//        let identifierToProcessData = ProcessStatusFilter.makeProcessDatas(output: output)
-//        XCTAssertEqual(identifierToProcessData.count, 1)
-//        guard let processData = identifierToProcessData[testProcessData.identifier] else {
-//            XCTAssertTrue(false)
-//            return
-//        }
-//
-//        XCTAssertEqual(processData.identifier, testProcessData.identifier)
-//        XCTAssertEqual(processData.startTime, testProcessData.startTime)
-//        XCTAssertEqual(processData.commandPath, testProcessData.commandPath)
-//    }
-//}
+@testable import SevenHup
+import SodaStream
+import XCTest
+
+class ProcessStatusFilterNoProcessTests: XCTestCase {
+    lazy var testProcessData: ProcessData = {
+
+        let identifier = Int32(74)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEE MMM d HH:mm:ss yyyy"
+        let startTime = dateFormatter.date(from: "Wed Dec 16 02:09:32 2015")!
+        let commandPath = "/usr/libexec/wdhelper"
+
+        let userInfo = ProcessManagerRouter.getUserInfo()
+        let userIdentifier = userInfo.userIdentifier
+        guard let username = userInfo.username else {
+            XCTFail()
+            return ProcessData(identifier: identifier,
+                               name: commandPath,
+                               userIdentifier: userIdentifier,
+                               username: "bad",
+                               startTime: startTime)!
+        }
+        return ProcessData(identifier: identifier,
+                           name: commandPath,
+                           userIdentifier: userIdentifier,
+                           username: username,
+                           startTime: startTime)!
+    }()
+
+    func testEmptyIdentifiers() {
+        let expectation = self.expectation(description: "Process filter finished")
+        ProcessStatusFilter.runningProcesses(withIdentifiers: [Int32]()) { (identifierToProcessData, error) -> Void in
+            XCTAssertNotNil(error)
+            XCTAssertNil(identifierToProcessData)
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: testTimeout, handler: nil)
+    }
+
+    func testEmptyInput() {
+        var processDatas = ProcessStatusFilter.makeProcessDatas(output: "")
+        XCTAssertEqual(processDatas.count, 0)
+        processDatas = ProcessStatusFilter.makeProcessDatas(output: " ")
+        XCTAssertEqual(processDatas.count, 0)
+    }
+
+    func testExampleInput() {
+        let fileURL = url(forResource: testDataTextPSOutputSmall,
+                          withExtension: testDataTextExtension,
+                          subdirectory: testDataSubdirectory)!
+
+        let output = makeString(contentsOf: fileURL)!
+
+        let identifierToProcessData = ProcessStatusFilter.makeProcessDatas(output: output)
+        XCTAssertEqual(identifierToProcessData.count, 3)
+        guard let processData = identifierToProcessData[testProcessData.identifier] else {
+            XCTAssertTrue(false)
+            return
+        }
+
+        XCTAssertEqual(processData.identifier, testProcessData.identifier)
+        XCTAssertEqual(processData.startTime, testProcessData.startTime)
+        XCTAssertEqual(processData.commandPath, testProcessData.commandPath)
+    }
+
+    func testBadExampleInput() {
+        let fileURL = url(forResource: testDataTextPSOutputBad,
+                          withExtension: testDataTextExtension,
+                          subdirectory: testDataSubdirectory)!
+
+        let output = makeString(contentsOf: fileURL)!
+
+        let identifierToProcessData = ProcessStatusFilter.makeProcessDatas(output: output)
+        XCTAssertEqual(identifierToProcessData.count, 1)
+        guard let processData = identifierToProcessData[testProcessData.identifier] else {
+            XCTAssertTrue(false)
+            return
+        }
+
+        XCTAssertEqual(processData.identifier, testProcessData.identifier)
+        XCTAssertEqual(processData.startTime, testProcessData.startTime)
+        XCTAssertEqual(processData.commandPath, testProcessData.commandPath)
+    }
+}
